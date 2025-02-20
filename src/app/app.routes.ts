b911934f93997a16ef30e4f8cba3e745b49029cf
AdemSansa <sansaadem@rades.r-iset.tn>
2025-02-20 18:02:39 +0100

    added features and needs a bit of update

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,7 +4,6 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
     
-    {path:'tasks',loadChildren:()=>import('./modules/tasks/tasks.route')},
     {path:'users',loadChildren:()=>import('./modules/users/users.route')},
     {path:'features',loadChildren:()=>import('./modules/features/features.route')}
 ];
